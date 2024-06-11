47c5bd4405c44968b3cee5f51f8c66696bd00c09
JiaGuangli <1511313969@qq.com>
2024-06-11 09:39:10 +0800

    feat:[Core]add string is null Extension

---
+++ src/Nm.Core/Extensions/StringExtension.cs
@@ -0,0 +1,8 @@
+﻿namespace Nm.Core.Extensions
+{
+    public static class StringExtension
+    {
+        public static bool IsNullOrWhiteSpace(this string? value) => string.IsNullOrWhiteSpace(value);
+        public static bool IsNullOrEmpty(this string? value) => string.IsNullOrEmpty(value);
+    }
+}
